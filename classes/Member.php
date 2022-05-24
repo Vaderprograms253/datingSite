14d68fc4a8fff13485df2b93865380dc018f3bc0
Xavier Denson <Denson.Xavier@student.greenriver.edu>
2022-05-23 21:12:55 -0400

    added class folder and files and modified nav on all pages

---
+++ classes/Member.php
@@ -0,0 +1,157 @@
+<?php
+
+/**
+ * Member.php
+ * Xavier Denson
+ * 05/23/2022
+ *
+ * This class represents a regular member
+ */
+class Member {
+    //instance variables
+    private $_firstName;
+    private $_lastName;
+    private $_age;
+    private $_gender;
+    private $_phone;
+    private $_email;
+    private $_state;
+    private $_seeking;
+    private $_bio;
+
+    public function __construct($_firstName, $_lastName, $_age, $_gender, $_phone) {
+        $this->_firstName = $_firstName;
+        $this->_lastName = $_lastName;
+        $this->_age = $_age;
+        $this->_gender = $_gender;
+        $this->_phone = $_phone;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName() {
+        return $this->_firstName;
+    }
+
+    /**
+     * @param mixed $firstName
+     */
+    public function setFirstName($firstName) {
+        $this->_firstName = $firstName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName() {
+        return $this->_lastName;
+    }
+
+    /**
+     * @param mixed $lastName
+     */
+    public function setLastName($lastName) {
+        $this->_lastName = $lastName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAge() {
+        return $this->_age;
+    }
+
+    /**
+     * @param mixed $age
+     */
+    public function setAge($age) {
+        $this->_age = $age;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGender() {
+        return $this->_gender;
+    }
+
+    /**
+     * @param mixed $gender
+     */
+    public function setGender($gender) {
+        $this->_gender = $gender;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhone() {
+        return $this->_phone;
+    }
+
+    /**
+     * @param mixed $phone
+     */
+    public function setPhone($phone) {
+        $this->_phone = $phone;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail() {
+        return $this->_email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email) {
+        $this->_email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getState() {
+        return $this->_state;
+    }
+
+    /**
+     * @param mixed $state
+     */
+    public function setState($state) {
+        $this->_state = $state;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSeeking() {
+        return $this->_seeking;
+    }
+
+    /**
+     * @param mixed $seeking
+     */
+    public function setSeeking($seeking) {
+        $this->_seeking = $seeking;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBio() {
+        return $this->_bio;
+    }
+
+    /**
+     * @param mixed $bio
+     */
+    public function setBio($bio) {
+        $this->_bio = $bio;
+    }
+
+
+}
