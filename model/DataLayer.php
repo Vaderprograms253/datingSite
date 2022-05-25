512d617b012f520707919deb85f21969aabda464
Xavier Denson <Denson.Xavier@student.greenriver.edu>
2022-05-25 19:38:49 -0400

    data layer and validation are now classes

---
+++ model/DataLayer.php
@@ -0,0 +1,14 @@
+<?php
+
+class DataLayer
+{
+    function getIndoor(){
+        return array("tv", "video games", "cooking", "reading", "decorating", "none" );
+    }
+
+    function getOutdoor(){
+        return array("hiking", "running", "exploring", "eating out", "bar", "none");
+
+    }
+
+}
